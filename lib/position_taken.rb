a555e163734e57c900b9dc8c8d622e35bfe42d06
Mary Bridget Bullock <bridget.bullock@wework.com>
2018-02-14 00:06:04 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,3 +1,10 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
+  board[index] =string
+  
+   #board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    #index = 0
+
+   # expect(position_taken?(board, index)).to be(false)
+ end
